5a3855ccf0d07870e8e1c56525ace13f755623b0
Jameson Taylor <jameson@ionic.io>
2024-03-15 14:08:24 -0400

    repro prep;

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,28 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { Geolocation } from '@capacitor/geolocation';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { useEffect } from 'react';
+
+let didInit = false;
 
 const Home: React.FC = () => {
+  const getCurrentPosition = async () => {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      console.log(coordinates.coords);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (!didInit) {
+      didInit = true;
+      getCurrentPosition();
+    }
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
